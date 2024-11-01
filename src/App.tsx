b668280593be934a40e0091cd3692d0d17123302
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-11-01 16:38:01 +0530

    [add] app .tsx code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import './App.css';
-
+import { useEffect, useState } from 'react';
 import { Controls } from './components/Controls';
 import { CurrentlyReading } from './components/CurrentlyReading';
+import { useSpeech } from './lib/useSpeech';
+import { fetchContent, parseContentIntoSentences } from './lib/content';
 
 function App() {
   const [sentences, setSentences] = useState<Array<string>>([]);
-  // const { currentWord, currentSentence, controls } = useSpeech(sentences);
+  const {currentSentenceIdx,currentWordRange,playbackState,play,pause}=useSpeech(sentences);
+  const loadNewContent=async ()=>{
+      const content=await fetchContent();
+      const parsedSentences=parseContentIntoSentences(content);
+      setSentences(parsedSentences);
+  }
+  useEffect(()=>{
+    loadNewContent();
+  },[])
 
   return (
     <div className="App">
       <h1>Text to speech</h1>
       <div>
-        <CurrentlyReading />
+        <CurrentlyReading currentWordRange={currentWordRange} currentSentenceIdx={currentSentenceIdx} sentences={sentences} />
       </div>
       <div>
-        <Controls/>
+        <Controls play={play} pause={pause} loadNewContent={loadNewContent} state={playbackState}/>
       </div>
     </div>
   );
