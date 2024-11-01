98f19e976db6aa13470a316a792a2312482002e5
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-11-01 16:14:19 +0530

    useSpeech: Hook that takes the current set of sentences and plays it using the speechEngine in speech.ts

--- src/lib/useSpeech.ts
+++ src/lib/useSpeech.ts
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState,useRef } from 'react';
 
-import { PlayingState } from './speech';
+import { createSpeechEngine, PlayingState } from './speech';
 
 /*
   @description
@@ -10,14 +10,63 @@ import { PlayingState } from './speech';
   This hook should return react friendly controls for playing, and pausing audio as well as provide information about
   the currently read word and sentence
 */
+
 const useSpeech = (sentences: Array<string>) => {
   const [currentSentenceIdx, setCurrentSentenceIdx] = useState(0);
   const [currentWordRange, setCurrentWordRange] = useState([0, 0]);
 
-  const [playbackState, setPlaybackState] = useState<PlayingState>("paused");
+  const [playbackState, setPlaybackState] = useState<PlayingState>("initialized");
 
-  const play = () => {};
-  const pause = () => {};
+  const engineRef=useRef(
+    createSpeechEngine({
+      onBoundary: (e:SpeechSynthesisEvent)=>{
+        if(e.name==='word')
+        {
+          setCurrentWordRange([e.charIndex,e.charIndex+e.charLength])
+        }},
+        onEnd:(e:SpeechSynthesisEvent)=>{
+          setCurrentSentenceIdx(prev=>{
+            const nextIdx=prev+1;
+            if(nextIdx<sentences.length)
+            {
+              engineRef.current.load(sentences[nextIdx]);
+              return nextIdx;
+            }
+            return prev;
+          });
+        },
+        onStateUpdate:(state:PlayingState)=>{
+          setPlaybackState(state);
+        }
+    })
+  );
+
+  useEffect(()=>{
+    if(sentences.length>0)
+      engineRef.current.load(sentences[0]);
+
+    return ()=>{
+      engineRef.current.cancel();
+    };
+  },[sentences]);
+
+  useEffect(()=>{
+    if(sentences.length>0)
+      engineRef.current.load(sentences[currentSentenceIdx]);
+  },[currentSentenceIdx,sentences]);
+
+  const play = () => {
+    if(currentSentenceIdx<sentences.length)
+      if(playbackState==="ended")
+      {
+        setCurrentSentenceIdx(0);
+        engineRef.current.load(sentences[0]);
+      }
+      engineRef.current.play();
+  };
+  const pause = () => {
+    engineRef.current.pause();
+  };
 
   return {
     currentSentenceIdx,
